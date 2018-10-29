4cd8392f86ba2421ce33ee2b3c1d200cd5af856a
akshay8149 <44441380+akshay8149@users.noreply.github.com>
2018-10-29 11:51:28 +0530

    Update Home.ts

--- Home.ts
+++ Home.ts
@@ -1,1 +1,14 @@
 
+import { Component,OnInit,Input } from '@angular/core';
+import {pipepage} from '../pipe';
+
+ @Component({
+  selector: 'page-about',
+  templateUrl: 'about.html'
+})
+
+export class AboutPage {
+   constructor() {
+   }
+ EnterText:any
+ }
